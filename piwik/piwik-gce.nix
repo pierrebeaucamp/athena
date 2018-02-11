aa60f0fbdb129318e2bcfbfd92a0ddfd772a8d4b
Pierre Beaucamp <mail@pierrebeaucamp.com>
2018-02-10 22:59:56 -0500

    Added Piwik tracking

---
+++ piwik/piwik-gce.nix
@@ -0,0 +1,49 @@
+{
+  network.description = "Piwik";
+
+  resources = {
+    gceNetworks.piwik-net = {
+      addressRange = "10.0.0.0/24";
+      firewall.allow-http.allowed.tcp = [ 80 443 ];
+    };
+
+    gceStaticIPs.public-ip.region = "northamerica-northeast1";
+  };
+
+  piwik = { resources, pkgs, ...}: {
+    networking = {
+      hostName = "piwik.pierebeaucamp.com";
+      firewall.allowedTCPPorts = [ 80 443 ];
+    };
+
+    deployment = {
+      targetEnv = "gce";
+      gce = {
+        region = "northamerica-northeast1-b";
+        instanceType = "f1-micro";
+        network = resources.gceNetworks.piwik-net;
+        ipAddress = resources.gceStaticIPs.public-ip;
+      };
+    };
+
+    services = {
+      nginx.enable = true;
+
+      piwik = {
+        enable = true;
+        nginx.default = true;
+      };
+
+      mysql = {
+        enable = true;
+        package = pkgs.mariadb;
+        initialScript = pkgs.writeText "piwikInit.sql" ''
+          INSTALL PLUGIN unix_socket SONAME 'auth_socket';
+          CREATE DATABASE piwik;
+          CREATE USER 'piwik'@'localhost' IDENTIFIED WITH unix_socket;
+          GRANT ALL PRIVILEGES ON piwik.* TO 'piwik'@'localhost';
+        '';
+      };
+    };
+  };
+}
