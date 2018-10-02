41807a42991f5e33f32e358129ab6fcc70f1cacc
Pierre Beaucamp <mail@pierrebeaucamp.com>
2018-10-01 20:05:47 -0400

    Installs git on CircleCI

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-apk add --update ca-certificates
+apk add --update ca-certificates git
 nix-channel --remove nixpkgs
 nix-channel --add https://nixos.org/channels/nixos-18.03 nixpkgs
 nix-channel --update
