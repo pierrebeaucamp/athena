cb5f402abc8ee2304afaa77833c07224028082de
Pierre Beaucamp <mail@pierrebeaucamp.com>
2018-10-01 20:30:21 -0400

    Use the correct variable as the neocities token

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 mkdir -p $HOME/.config/neocities
-echo $NEOCITIES >> $HOME/.config/neocities/config
+echo $NEOCITIES_TOKEN >> $HOME/.config/neocities/config
 
 neocities push result
